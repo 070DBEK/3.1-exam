b958e953b062e13c894486ab81dc6777eed5cd8c
Ozodbek <ahmadovozodbek80@gmail.com>
2025-03-15 20:03:29 +0500

    Admin panel added!

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude', 'elevation', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import WeatherData, Forecast
 
-# Register your models here.
+
+@admin.register(WeatherData)
+class WeatherDataAdmin(admin.ModelAdmin):
+    list_display = ('location', 'temperature', 'humidity', 'pressure', 'wind_speed', 'wind_direction', 'precipitation', 'recorded_at')
+    search_fields = ('location__name',)
+    list_filter = ('recorded_at', 'location')
+    ordering = ('-recorded_at',)
+
+
+@admin.register(Forecast)
+class ForecastAdmin(admin.ModelAdmin):
+    list_display = ('location', 'forecast_date', 'temperature_min', 'temperature_max', 'humidity', 'precipitation_probability', 'wind_speed', 'wind_direction', 'created_at')
+    search_fields = ('location__name',)
+    list_filter = ('forecast_date', 'location')
+    ordering = ('-forecast_date',)
